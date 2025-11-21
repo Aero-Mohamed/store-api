db654d58458f580679023134c89376bdc629056e
Mohamed Ahmed <aero.mohamed@icloud.com>
2025-11-21 18:17:53 +0200

    feat: adding sorting to getAllusers

--- src/main/java/com/hassan/store/controllers/UserController.java
+++ src/main/java/com/hassan/store/controllers/UserController.java
@@ -5,14 +5,13 @@ import com.hassan.store.entities.User;
 import com.hassan.store.mappers.UserMapper;
 import com.hassan.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @AllArgsConstructor
 @RestController
@@ -22,10 +21,19 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping()
-    public List<UserDto> getAllUsers(){
-        return userRepository.findAll().stream()
+    public ResponseEntity<List<UserDto>> getAllUsers(
+        @RequestParam(required = false, defaultValue = "", name="sort") String sort
+    ){
+        if(!Set.of("name", "email").contains(sort)){
+            sort = "id";
+        }
+
+        var users = userRepository.findAll(Sort.by(sort))
+                .stream()
                 .map(userMapper::toDto)
                 .toList();
+
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/hassan/store/mappers/UserMapper.java
+++ src/main/java/com/hassan/store/mappers/UserMapper.java
@@ -3,8 +3,11 @@ package com.hassan.store.mappers;
 import com.hassan.store.dtos.UserDto;
 import com.hassan.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
     UserDto toDto(User user);
+
 }
