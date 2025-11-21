36dad7f8c95136a50aa829d1daeb4ca780dc1c3e
Mohamed Ahmed <aero.mohamed@icloud.com>
2025-11-21 20:50:36 +0200

    feat: validating create user request

--- src/main/java/com/hassan/store/dtos/CreateUserRequest.java
+++ src/main/java/com/hassan/store/dtos/CreateUserRequest.java
@@ -1,10 +1,23 @@
 package com.hassan.store.dtos;
 
+import com.hassan.store.validation.Lowercase;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateUserRequest {
+    @NotBlank(message = "Name is required")
+    @Size(max=255, message = "Name must be less than 255 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is not valid")
+    @Lowercase(message = "Email must be in lowercase")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min=8, max=255, message = "Password must be between 8 and 255 characters")
     private String password;
 }
