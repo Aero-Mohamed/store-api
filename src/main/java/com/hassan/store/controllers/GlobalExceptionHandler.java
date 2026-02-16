c71b1a14e9c4dc97d32f082e7a7a0caca7385567
Mohamed Ahmed <aero.mohamed@icloud.com>
2026-02-16 09:38:25 +0200

    feat: handle exception

--- src/main/java/com/hassan/store/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/hassan/store/controllers/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.hassan.store.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +11,14 @@ import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    // e.g., when fail to parse "123" as UUID
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleUnreadableMessage(Exception exception){
+        return ResponseEntity.badRequest().body(
+                Map.of("error", "Invalid Request body")
+        );
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationErrors(
